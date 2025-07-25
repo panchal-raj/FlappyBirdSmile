2e5226972b01af1b6cb273fd6841404da2175ee5
panchal-raj <panchal.raj.work@gmail.com>
2025-07-25 16:40:07 +0200

    Increased the Threshold for SmileDection.

--- static/js/smileDetector.js
+++ static/js/smileDetector.js
@@ -6,7 +6,7 @@ import { getVideoElement } from './webcam.js';
 let faceMesh;
 let lastSmileScore = 0;
 // This threshold can be adjusted during a calibration step.
-let smileThreshold = 4.0; 
+let smileThreshold = 4.5; 
 
 let faceApiLoaded = false;
 
